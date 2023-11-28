1f4052b6484f495578789b5141cbaa7955bf7c87
Everton Nascimento <nascimentoeverton568@gmail.com>
2023-11-28 19:54:21 -0300

    Atualizações

--- src/main/java/com/urbtech/config/CorsConfig.java
+++ src/main/java/com/urbtech/config/CorsConfig.java
@@ -6,32 +6,39 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/usuario/**")
-                        .allowedOrigins("https://urbtech.netlify.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+public class CorsConfig implements WebMvcConfigurer{
 
-                registry.addMapping("/login/**")
-                        .allowedOrigins("https://urbtech.netlify.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
-
-                registry.addMapping("/post/**")
-                        .allowedOrigins("https://urbtech.netlify.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
-
-                registry.addMapping("/comunidade/**")
-                        .allowedOrigins("https://urbtech.netlify.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
-
-                registry.addMapping("/comentario/**")
-                        .allowedOrigins("https://urbtech.netlify.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
+
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/usuario/**")
+//                        .allowedOrigins("*")
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+//
+//                registry.addMapping("/login/**")
+//                        .allowedOrigins("*")
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+//
+//                registry.addMapping("/post/**")
+//                        .allowedOrigins("*")
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+//
+//                registry.addMapping("/comunidade/**")
+//                        .allowedOrigins("*")
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+//
+//                registry.addMapping("/comentario/**")
+//                        .allowedOrigins("*")
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+//            }
+//        };
 }
