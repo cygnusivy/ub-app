ce4decf4b8396d734a9a1386ac24f3aec546763f
Everton Nascimento <nascimentoeverton568@gmail.com>
2023-11-28 21:22:51 -0300

    Atualizações

--- src/main/java/com/urbtech/UrbtechApiApplication.java
+++ src/main/java/com/urbtech/UrbtechApiApplication.java
@@ -2,12 +2,13 @@ package com.urbtech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.urbtech.config")
-public class UrbtechApiApplication {
+public class UrbtechApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 
